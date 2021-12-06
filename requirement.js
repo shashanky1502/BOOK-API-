d5081b92f303b94a1e8bd0e01c00f8c17d0ff078
Shashank Yadav <84345917+shashanky1502@users.noreply.github.com>
2021-12-06 08:10:32 +0530

    Add files via upload

---
+++ requirement.js
@@ -0,0 +1,43 @@
+//Requirements for our project
+
+//Book management company
+
+//BOOKS
+//ISBN, title, pub date, language, num page, author[], publication[], category[]
+
+//AUTHORS
+//id, name, books[]
+
+//PUBLICATIONS
+//id, name, books[]
+
+//What are the APIs we need?
+
+//BOOKS
+//WE NEED AN API FOR
+//-> to get all books - DONE
+//-> to get specific book - DONE
+//-> to get a list of books based on category - DONE
+//-> to get a list of books based on languages - ASSIGNMENT
+
+//AUTHORS
+//WE NEED AN API FOR
+//-> to get all authors - DONE
+//-> to get specific author - DONE
+//-> to get a list of authors based on books - ASSIGNMENT
+
+//PUBLICATIONS
+//WE NEED AN API FOR
+//-> to get all publications - DONE
+//-> to get specific publication - ASSIGNMENT
+//-> to get a list of publications based on book - ASSIGNMENT
+
+//POST REQUEST
+//-> Add new books - DONE
+//-> Add new authors - DONE
+//-> Add new publications - DONE
+
+//PUT REQUEST
+
+
+//DELETE REQUEST
